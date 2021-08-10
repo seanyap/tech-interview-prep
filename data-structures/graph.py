7c22f59fc793bb2812f851c50163738543c978ef
Sean Yap <syap0317@gmail.com>
2021-08-09 22:58:19 -0400

    added more comments on graph

--- data-structures/graph.py
+++ data-structures/graph.py
@@ -1,7 +1,11 @@
+# Graph
+# -> picture a node that has a connection to another node aka neighbor
+# -> in its simplest form, it has at least 2 nodes with 1 connection between them
+#
 # Graph can be represented 2 ways:
 #   1. Adjacency List 
-#      -> a dictionary of all nodes (vertices), and for each node key,
-#         a list of the nodes that it connects to
+#      -> a dictionary of all nodes(vertices), and for each node key,
+#         a list of the nodes that it connects to (neighbors)
 #      -> efficiently represent sparse graphs
 #      -> Space = O(V + E)
 #      -> Operations Run time
@@ -10,8 +14,8 @@
 #         -> Deleting an edge = O(V)
 #
 #   2. Adjacency Matrix
-#      -> a V x V matrix using 2D array with a boolean value indicating
-#         if there is an edge between two vertices
+#      -> a V x V matrix(represented by 2D array) with a boolean value 
+#         indicating if there is an edge(connection) between two vertices
 #      -> efficiently represent dense graphs
 #      -> Space = O(V x V)
 #      -> Operations Run time
